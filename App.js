ef7a3c75bdca42a5d2f075e9d2be95450693b28b
siraj9949 <shaiksirajuddin9949@gmail.com>
2023-03-05 10:44:24 +0530

    eslint

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text,SafeAreaView, View,Platform,StatusBar as StBar } from 'react-native';
+/* eslint-disable react/react-in-jsx-scope */
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Platform,
+  StatusBar as StBar,
+} from "react-native";
 import { colors } from "./src/utils/colors";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={{paddingBottom:10,backgroundColor:colors.green,padding:16}}>Search</Text>
-      <Text style={{backgroundColor:colors.blue,flex:1,padding:16}}>List</Text>
+      <Text style={styles.search}>Search</Text>
+      <Text style={styles.list}>List</Text>
     </SafeAreaView>
   );
 }
@@ -14,8 +21,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS ==="android" ?StBar.currentHeight:0,
-    
-
+    paddingTop: Platform.OS === "android" ? StBar.currentHeight : 0,
   },
+  search: {
+    paddingBottom: 10,
+    backgroundColor: colors.green,
+    padding: 16,
+  },
+  list: { backgroundColor: colors.blue, flex: 1, padding: 16 },
 });
