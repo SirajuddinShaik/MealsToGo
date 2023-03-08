350d82bebfdaccc127c4c119435613ea0d237bae
siraj9949 <shaiksirajuddin9949@gmail.com>
2023-03-08 15:21:38 +0530

    nothing

--- src/features/restaurents/components/restaurant-info-card.component.js
+++ src/features/restaurents/components/restaurant-info-card.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 
@@ -34,39 +34,67 @@ const Title = styled(Text)`
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4.6,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const rattingArray = Array.from(new Array(Math.round(rating)));
 
-  const Rating = styled(View)`
+  const RestDetails = styled(View)`
     flex-direction: row;
-    padding: ${(props) => props.theme.space[2]} 0;
   `;
   const IsOpen = styled(View)`
     position: absolute;
-    right: ${(props) => props.theme.space[2]};
+    right: 36px;
+    padding: ${(props) => props.theme.space[2]} 0;
   `;
+  const IsClosedTemporarily = styled(Text)`
+    padding: ${(props) => props.theme.space[2]} 0;
+    position: absolute;
+    right: ${(props) => props.theme.space[5]};
+    font-family: ${(props) => props.theme.fonts.heading};
+    font-size: ${(props) => props.theme.fontSizes.caption};
+    color: ${(props) => props.theme.colors.ui.error};
+  `;
+  const RestTypeImage = styled(View)`
+    position: absolute;
+    right: ${(props) => props.theme.space[2]};
+    padding: ${(props) => props.theme.space[2]} 0;
+  `;
+  const Rating = styled(Text)`
+    padding: ${(props) => props.theme.space[2]} 0;
+  `;
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
-        <Rating>
-          {rattingArray.map(() => (
-            <SvgXml xml={star} height={20} width={20} />
-          ))}
+        <RestDetails>
+          <Rating>
+            {rattingArray.map(() => (
+              <SvgXml xml={star} height={20} width={20} />
+            ))}
+          </Rating>
+
           <IsOpen>
             {isOpenNow && <SvgXml xml={open} height={20} width={20} />}
           </IsOpen>
-        </Rating>
+
+          {isClosedTemporarily && (
+            <IsClosedTemporarily>CLOSED TEMPORARILY</IsClosedTemporarily>
+          )}
+          <Image source={{ uri: icon }} height={20} />
+          <RestTypeImage>
+            <Image source={{ uri: icon }} height={20} width={20} />
+          </RestTypeImage>
+        </RestDetails>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
